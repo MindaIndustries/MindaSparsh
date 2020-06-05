dd61af4aee6a9fb5709ae603c8033b7334fa32ae
varuna <vmahendru@mindagroup.com>
2020-06-05 16:20:43 +0530

    master

--- app/src/main/java/com/minda/sparsh/EHSInitiate.java
+++ app/src/main/java/com/minda/sparsh/EHSInitiate.java
@@ -1,15 +1,31 @@
 package com.minda.sparsh;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class EHSInitiate extends BaseActivity{
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.title)
+    TextView title;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ehs_initiate);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back);
+        title.setText("EHS Initiate");
+
     }
 }
